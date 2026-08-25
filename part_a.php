5c7f7cac9b3391d1aeb52d6fbe21c2c1cde4b2db
ArianFaraz-Developer <farazarian3@gmail.com>
2026-08-25 22:23:40 +0430

    Add files via upload

---
+++ part_a.php
@@ -0,0 +1,17 @@
+<?php
+// Part A
+class Student
+{
+    function sayHello()
+    {
+        echo "Hello! I am a student.";
+    }
+}
+
+$student1 = new Student();
+
+$student1->sayHello();
+
+
+
+?>
